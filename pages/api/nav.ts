acf7ea10799676b106f8aaf7ff0e73c916540062
linzhe <linzhe141@qq.com>
2023-10-08 10:50:05 +0800

    [test] 测试pages路由

---
+++ pages/api/nav.ts
@@ -0,0 +1,44 @@
+import fs from "fs/promises";
+import path from "path";
+import { NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
+export async function GET(request: Request) {
+  const test = [];
+  for (const name of await fs.readdir(
+    path.resolve(process.cwd(), "app/blog/data_structure/tree")
+  )) {
+    test.push(name);
+  }
+  const result = {
+    code: 200,
+    test: test,
+  };
+  return NextResponse.json(result);
+}
+
+export async function POST(request: Request) {
+  return NextResponse.json({ data: true });
+}
+// export const config = {
+//   runtime: "edge",
+// };
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const test = [];
+    for (const name of await fs.readdir(
+      path.resolve(process.cwd(), "app/blog/data_structure/tree")
+    )) {
+      test.push(name);
+    }
+    const result = {
+      code: 200,
+      test: test,
+    };
+    return res.json(result);
+  } else if (req.method === "POST") {
+    return res.json({ data: true });
+  }
+}
